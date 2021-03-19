c84a3a2e4112c65b65ab41e5b087d84d2f8d443f
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-03-19 11:22:11 -0300

    setima versao

--- src/calculos/Catalogo.jsx
+++ src/calculos/Catalogo.jsx
@@ -10,56 +10,59 @@ import Rescisao from '../imagens/rescicao.jpg'
 export default function(){
     return(
         <div>
-            <div className="card">
-                <div className="card-header">
+            <div className="card cardmain">
+                <div className="card-header cardtitle">
                     <h2>Catalogo de calculos trabalhistas</h2>
                 </div>
             <div className="row">
                 <div className="col-md-3">
-                    <div className="card">
+                    <div className="card cardmain">
                         <div className='card-body'>
                             <img src={ Ferias }  className="fly"/>
                         </div>
-                        <div className="card-footer">
-                            <h2 className="h4">Ferias</h2>
+                        <div className="card-footer cardfooter">
+                            <a href='#'><h2 className="h4">Ferias</h2></a>
                         </div>
 
                     </div>
                 </div>
                 <div className="col-md-3">
-                    <div className="card">
+                    <div className="card cardmain">
                         <div className='card-body'>
                              <img src={ Decimoterceiro } className="fly"/>
                         </div>
-                        <div className="card-footer">
-                            <h2 className="h4">13º Salario</h2>
+                        <div className="card-footer cardfooter">
+                                <a href='#'><h2 className="h4">13º Salario</h2></a>
                         </div>
 
                     </div>
                 </div>
                 <div className="col-md-3">
-                    <div className="card">
+                    <div className="card cardmain">
                         <div className='card-body'>
                             <img src={ Folha }  className="fly"/>
                         </div>
-                        <div className="card-footer">
-                            <h2 className="h4">Folha de pgto</h2>
+                        <div className="card-footer cardfooter">
+                            <a href='#'><h2 className="h4">Folha de pgto</h2></a>
                         </div>
 
                     </div>
                 </div>
                 <div className="col-md-3">
-                    <div className="card">
+                    <div className="card cardmain">
                         <div className='card-body'>
                             <img src={ Rescisao }  className="fly"/>
                         </div>
-                        <div className="card-footer">
-                            <h2 className="h4">Rescisao Contratual</h2>
+                        <div className="card-footer cardfooter">
+                            <a href='#'><h2 className="h5">Rescisao Contratual</h2></a>
                         </div>
 
                     </div>
                 </div>
 
+            </div>
+            <div className="card-footer footer">
+                <p>Myadmin acessoria em Departamento pessoal</p>
             </div>
             </div> 
 
