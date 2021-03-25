9e5a6e1bd9752cd8afc040d193e43f59867653ef
Rodrigo Oliveira <frejat.rodrigo@gmail.com>
2021-03-25 16:32:35 -0300

    decima segunda versao

--- src/ferias/ferias.jsx
+++ src/ferias/ferias.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 import './ferias.css'
 
+
 export default function(){
+    const [sb, setSb ] = useState()
+    const [dias, setDias ] = useState()
+    const [adc, setAdc ] = useState()
+    const [vp, setVp ] = useState()
+    const [terco, setTerco ] = useState()
+    const [total, setTotal ] = useState()
+
+    const calcularferias = () => {
+        const vsb = parseFloat(sb)
+        const vdias = parseInt(dias)
+        const vadc = parseFloat(adc)
+        const vvp = (( vsb + vadc) /30 * vdias )
+        const vterco = (vvp / 3)
+        
+        setVp( vvp )
+
+        setTerco( vterco )
+
+        setTotal( vvp + vterco )
+
+    }
+
+  
+
     return(
         <div className="card carddefault">
             <div className="card-header cardferias">
@@ -11,21 +36,26 @@ export default function(){
                 <div className="row">
                     <div className="col-md-6">
                         <label className="form-label">Nome completo do colaborador:</label><br></br>
-                        <input type="text" class="form-control" autoFocus required></input>
+                        <input type="text" class="form-control" autoFocus required ></input>
                         <label className="form-label">Informe Salario bruto do colaborador:</label><br></br>
-                        <input type="text" class="form-control" required></input>
+                        <input type="text" class="form-control" required value={sb} 
+                            onChange={e => setSb(e.target.value)}></input>
                         <label className="form-label">Informe a qtde de dias a usufruir de ferias:</label><br></br>
-                        <input type="text" class="form-control" required></input><br></br>
-                        <button className="btn btn-outline-primary">Calcular</button>
+                        <input type="text" class="form-control" required value={dias} 
+                            onChange={e => setDias(e.target.value)}></input><br></br>
+                        <label className="form-label">Informe Media de adicional:</label><br></br>
+                        <input type="text" class="form-control" required value={adc} 
+                            onChange={e => setAdc(e.target.value)}></input><br></br>
+                        <button className="btn btn-outline-primary" onClick={calcularferias}>Calcular</button>
                     </div>
                     <div className="col-md-6">
                         <label className="form-label">Valor proporcional de ferias:</label><br></br>
-                        <input type="text" class="form-control"></input><br></br>
+                        <input type="text" class="form-control" value={vp}></input><br></br>
                         <label className="form-label">Valor do terço de ferias:</label><br></br>
-                        <input type="text" class="form-control"></input><br></br>
+                        <input type="text" class="form-control" value={terco}></input><br></br>
                         <label className="form-label">Valor de total de ferias</label><br></br>
-                        <input type="text" class="form-control"></input><br></br>
-                     
+                        <input type="text" class="form-control" value={total}></input><br></br>
+                        
                     </div>
 
                 </div>
